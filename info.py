617aa543ba77858fb340713836ce6fe381ea1014
TmrBotz <tmrbotz@gmail.com>
2025-02-28 19:25:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,9 +68,9 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tmrbotz:nym4CaS2XZDTfO4S@cluster0.punis.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://tmrbotz:rF0qfwuG7tvOEZq8@cluster0.fs4vj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://tmrbotz:XkhGZbR0gf3jRBSU@cluster0.dc9lh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Pushpa_Db1")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
 # ============================
 # Movie Notification & Update Settings
