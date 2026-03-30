6b696607bb7024f77c87115062b859fd16f5a788
Tmrbotz <tmrbotz@gmail.com>
2026-03-30 18:09:55 +0530

    Change default value of IS_SHORTLINK to False

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/How_to_Download_7x30'
 # ============================
 # Link Shortener Configuration
 # ============================
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkpays.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '8f2024d7bb019ff4ce635616ecdc9a751b09bb58')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_2_Dl')  # Tutorial video link for opening shortlink website
